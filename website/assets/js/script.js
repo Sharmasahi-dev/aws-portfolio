6d9cd5a064b5677e7434f1810ffbf48cd2dd98b7
Sahil Sharma <sahil.t.sharma2@gmail.com>
2026-05-07 16:53:33 +0900

    updte earthmap

--- website/assets/js/script.js
+++ website/assets/js/script.js
@@ -1,3 +1,7 @@
+/* =========================
+   MOBILE MENU
+========================= */
+
 const menuToggle = document.getElementById("menu-toggle");
 
 const navLinks = document.querySelector(".nav-links");
@@ -7,3 +11,33 @@ menuToggle.addEventListener("click", () => {
     navLinks.classList.toggle("active");
 
 });
+
+/* =========================
+   3D EARTH GLOBE
+========================= */
+
+const globeContainer = document.getElementById("globeViz");
+
+if (globeContainer) {
+
+    const world = Globe()
+
+    (globeContainer)
+
+    .globeImageUrl('//unpkg.com/three-globe/example/img/earth-night.jpg')
+
+    .bumpImageUrl('//unpkg.com/three-globe/example/img/earth-topology.png')
+
+    .backgroundColor('#000000')
+
+    .showAtmosphere(true)
+
+    .atmosphereColor('#3fa9ff')
+
+    .atmosphereAltitude(0.25);
+
+    world.controls().autoRotate = true;
+
+    world.controls().autoRotateSpeed = 0.7;
+
+}
